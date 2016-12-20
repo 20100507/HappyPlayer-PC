08e397e9ba4a0dbcc98d2c7d5d24415f40f8c804
zhangliangming <316257874@qq.com>
2016-12-21 01:23:03 +0800

    添加了ksc歌词的支持

--- src/com/happy/util/LyricsUtil.java
+++ src/com/happy/util/LyricsUtil.java
@@ -1,10 +1,12 @@
 package com.happy.util;
 
 import java.io.File;
+import java.util.List;
 
 import javax.swing.SwingWorker;
 
 import com.happy.common.Constants;
+import com.happy.lyrics.system.LyricsInfoIO;
 import com.happy.model.SongMessage;
 import com.happy.observable.ObserverManage;
 
@@ -32,22 +34,10 @@ public class LyricsUtil {
 			@Override
 			protected Void doInBackground() {
 
-				String lrcFilePath = Constants.PATH_LYRICS + File.separator
-						+ displayName + ".hrc";
-
-				File lrcFile = new File(lrcFilePath);
-				if (!lrcFile.exists()) {
-
-					lrcFilePath = Constants.PATH_LYRICS + File.separator
-							+ displayName + ".ksc";
-
-					lrcFile = new File(lrcFilePath);
-					if (!lrcFile.exists()) {
-						return null;
-					}
-
+				File lrcFile = getLrcFile(displayName);
+				if (lrcFile == null) {
+					return null;
 				}
-
 				SongMessage songMessage = new SongMessage();
 
 				if (type == SongMessage.LRCTYPELRC) {
@@ -61,13 +51,13 @@ public class LyricsUtil {
 					songMessage.setType(SongMessage.LOCKLOADED);
 				}
 
-				songMessage.setLrcFilePath(lrcFilePath);
+				songMessage.setLrcFilePath(lrcFile.getPath());
 				songMessage.setSid(sid);
 				// 通知
 				ObserverManage.getObserver().setMessage(songMessage);
 
 				return null;
-				
+
 			}
 
 			@Override
@@ -75,5 +65,25 @@ public class LyricsUtil {
 
 			}
 		}.execute();
+	}
+
+	/**
+	 * 通过音频文件名获取歌词文件
+	 * 
+	 * @param displayName
+	 * @return
+	 */
+	public static File getLrcFile(String displayName) {
+		File lrcFile = null;
+		List<String> lrcExts = LyricsInfoIO.getSupportLyricsExts();
+		for (int i = 0; i < lrcExts.size(); i++) {
+			String lrcFilePath = Constants.PATH_LYRICS + File.separator
+					+ displayName + "." + lrcExts.get(i);
+			lrcFile = new File(lrcFilePath);
+			if (lrcFile.exists()) {
+				break;
+			}
+		}
+		return lrcFile;
 	}
 }
